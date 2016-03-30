4fcce9beab73582820558182b2b7efbc29097eb0
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2016-03-30 10:57:26 -0300

    ...

---
+++ sc-utils-java/src/test/java/br/com/softctrl/utils/java/GSonUtilsTest.java
@@ -0,0 +1,34 @@
+package br.com.softctrl.utils.java;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GSonUtilsTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName
+     *            name of the test case
+     */
+    public GSonUtilsTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(GSonUtilsTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
